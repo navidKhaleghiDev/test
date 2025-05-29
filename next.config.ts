5b0a60d7fae0d50e89e37dc97dbff739fb4b71c9
navidKhaleghiDev <navidkhaleghi669@gmail.com>
2025-05-29 23:20:31 +0330

    init: config eslint & prettier

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint:{
+    // this configures that where to use the eslint in the project and we can disable in the build time. 
+    // dirs:["app"],
+    // ignoreDuringBuilds : true
+  },
 };
+
+
 
 export default nextConfig;
